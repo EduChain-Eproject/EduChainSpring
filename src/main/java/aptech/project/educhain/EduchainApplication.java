a02a00532b8f5a762c09083150c87c0195c5b6e4
Pham Tien Duc <phamtienduceng@gmail.com>
2024-05-21 14:56:25 +0700

    initial commit

---
+++ src/main/java/aptech/project/educhain/EduchainApplication.java
@@ -0,0 +1,13 @@
+package aptech.project.educhain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduchainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aptech/project/educhain/EduchainApplicationTests.java
@@ -0,0 +1,13 @@
+package aptech.project.educhain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduchainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
