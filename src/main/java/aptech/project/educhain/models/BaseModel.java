185a5ec5f956b88f9f15beaea4922d27bc42b4da
Pham Tien Duc <phamtienduceng@gmail.com>
2024-05-22 17:16:00 +0700

    update model

--- src/main/java/aptech/project/educhain/models/BaseModel.java
+++ src/main/java/aptech/project/educhain/models/BaseModel.java
@@ -7,27 +7,16 @@ import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 
-<<<<<<< Updated upstream
 @MappedSuperclass
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-=======
-@Data
-@MappedSuperclass
-@NoArgsConstructor
-@AllArgsConstructor
->>>>>>> Stashed changes
 public class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
 
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     @Column(name = "created_at")
     private Timestamp createdAt;
 }
