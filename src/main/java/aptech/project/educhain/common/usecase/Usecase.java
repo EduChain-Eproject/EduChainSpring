4f8746e29422b139f8c8204f8033a7d042029bda
Ganzzi <boinguyen9701@gmail.com>
2024-06-19 00:33:29 +0700

    update usecase interface

--- src/main/java/aptech/project/educhain/common/usecase/Usecase.java
+++ src/main/java/aptech/project/educhain/common/usecase/Usecase.java
@@ -1,5 +1,7 @@
 package aptech.project.educhain.common.usecase;
 
+import aptech.project.educhain.common.result.AppResult;
+
 public interface Usecase<SuccessType, NoParam> {
-    public SuccessType execute(NoParam params);
+    public AppResult<SuccessType> execute(NoParam params);
 }
